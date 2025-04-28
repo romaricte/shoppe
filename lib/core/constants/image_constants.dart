92efb4b124fa5f9299d882d749bffb73b436daf0
romaricte <tenrom142@gmail.com>
2025-04-28 19:56:54 +0100

    image_profile

--- lib/core/constants/image_constants.dart
+++ lib/core/constants/image_constants.dart
@@ -11,6 +11,18 @@ class ImageConstants {
   static const String createAccount = 'assets/images/auth/createAccount.png';
   static const String login = 'assets/images/auth/login.png';
  
+  // images/home
+  static const String home = 'assets/images/home/home.png';
+  static const String newitems1 = 'assets/images/home/newitems1.png';
+  static const String newitems2 = 'assets/images/home/newitems2.png';
+  static const String storie1 = 'assets/images/home/storie1.png';
+  static const String storie2 = 'assets/images/home/storie2.png';
+  static const String storie3 = 'assets/images/home/storie3.png';
+  static const String viewed1 = 'assets/images/home/viewed1.png';
+  static const String viewed2 = 'assets/images/home/viewed2.png';
+  static const String viewed3 = 'assets/images/home/viewed3.png';
+  static const String viewed4 = 'assets/images/home/viewed4.png';
+  static const String viewed5 = 'assets/images/home/viewed5.png';
   
   
   
