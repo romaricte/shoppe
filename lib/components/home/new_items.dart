6c6dd49fe7749277a1748cd01a8168be7b9a1ad2
romaricte <tenrom142@gmail.com>
2025-05-04 22:32:21 +0100

    component

---
+++ lib/components/home/new_items.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewItems extends StatefulWidget {
+  const NewItems({super.key});
+
+  @override
+  State<NewItems> createState() => _NewItemsState();
+}
+
+class _NewItemsState extends State<NewItems> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
